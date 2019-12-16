67f2fb90307450fddb3206bcbe2c087547bd772c
Maria22 <m.ushakova22@gmail.com>
2019-12-16 22:56:08 +0100

    Auto packages css

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,22 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var del = require("del");
 
-gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
+gulp.task("css", function() {
+  return gulp
+    .src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("server", function () {
+gulp.task("server", function() {
   server.init({
     server: "source/",
     notify: false,
@@ -34,4 +35,15 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("del", function() {
+  return del("build");
+});
+gulp.task("copy", function() {
+  return gulp
+    .src(["source/fonts/**/*.{woff,woff2}", "source/img/**", "source/js/**"], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+gulp.task("build", gulp.series("del", "copy", "css"));
+gulp.task("start", gulp.series("build", "server"));
