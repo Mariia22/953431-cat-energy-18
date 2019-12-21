cd010fece1fa465a7ad7c205ac909a1a76e7a6a7
Maria22 <m.ushakova22@gmail.com>
2019-12-21 18:12:25 +0100

    Add slider

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,11 @@
-var navMain = document.querySelector(".page-nav");
-var navToggle = document.querySelector(".page-nav__toggle");
+let navMain = document.querySelector(".page-nav");
+let navToggle = document.querySelector(".page-nav__toggle");
+let beforeButton = document.querySelector(".example__button-before");
+let afterButton = document.querySelector(".example__button-after");
+let beforePage = document.querySelector(".example__photo-before");
+let afterPage = document.querySelector(".example__photo-after");
+let scale = document.querySelector(".example__scale");
+let map;
 
 navMain.classList.remove("page-nav--nojs");
 
@@ -12,7 +18,26 @@ navToggle.addEventListener("click", function () {
     navMain.classList.remove("page-nav--opened");
   }
 });
-var map;
+
+beforeButton.addEventListener("click", function () {
+  if (beforePage.classList.contains("example__photo-closed")) {
+    beforePage.classList.remove("example__photo-closed");
+    beforePage.classList.add("example__photo-opened");
+    afterPage.classList.remove("example__photo-opened");
+    afterPage.classList.add("example__photo-closed");
+    scale.classList.remove("example__scale-after");
+  }
+});
+
+afterButton.addEventListener("click", function () {
+  if (afterPage.classList.contains("example__photo-closed")) {
+    afterPage.classList.remove("example__photo-closed");
+    afterPage.classList.add("example__photo-opened");
+    beforePage.classList.remove("example__photo-opened");
+    beforePage.classList.add("example__photo-closed");
+    scale.classList.add("example__scale-after");
+  }
+});
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
